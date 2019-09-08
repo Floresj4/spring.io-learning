da4a52353e9d3d7daa8c980f7b48176ebd1356d6
Jason <floresj4@gmail.com>
2019-09-07 20:41:31 -0400

    inital commit

---
+++ src/main/java/com/flores/projects/springboot/Application.java
@@ -0,0 +1,13 @@
+package com.flores.projects.springboot;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
