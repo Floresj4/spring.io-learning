cfdb24cd1283a439139f156a9c2db8fb62d711d5
Jason <floresj4@gmail.com>
2019-09-15 19:56:13 -0400

    registering filter and mock/sample auth

--- src/main/java/com/flores/projects/spring/config/WebSecurityConfig.java
+++ src/main/java/com/flores/projects/spring/config/WebSecurityConfig.java
@@ -1,33 +1,50 @@
 package com.flores.projects.spring.config;
 
-import org.springframework.context.annotation.Bean;
+import static java.lang.String.valueOf;
+
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+
+import com.flores.projects.spring.filter.AuthorizationHeaderFilter;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Bean @SuppressWarnings("deprecation")
-    public UserDetailsService userDetailsService() {
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();       
-        manager.createUser(users.username("admin").password("123").roles("USER", "ADMIN").build());
-        manager.createUser(users.username("user").password("123").roles("USER").build());
-
-        return manager;
-    }
-    
     protected void configure(HttpSecurity http) throws Exception {
+    	//initialize the authorization filter and register with HttpSecurity
+    	AuthorizationHeaderFilter authHeaderFilter = new AuthorizationHeaderFilter();
+    	authHeaderFilter.setAuthenticationManager(new AuthenticationManager() {
+
+			@Override
+			public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+				String principal = valueOf(authentication.getPrincipal());
+				authentication.setAuthenticated(principal != null);
+				return authentication;
+			}
+    	});
+    	
     	http.authorizeRequests()
     		.antMatchers("/public/**").permitAll()
-    		.anyRequest().authenticated()
-    		.and().formLogin();
+    		.and()
+    			.sessionManagement()
+    				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    		.and()
+    			.addFilter(authHeaderFilter)
+    			.addFilterBefore(new ExceptionTranslationFilter(
+                        new Http403ForbiddenEntryPoint()),
+    						authHeaderFilter.getClass()
+                    )
+    			.authorizeRequests()
+    				.anyRequest()
+    				.authenticated();
     }
 }
